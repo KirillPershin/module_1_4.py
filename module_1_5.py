634374aa4cfc1814201720e005f5ef87c0ca136f
KirillPershin <Kirillpersin987@gmail.com>
2024-10-21 14:38:06 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+my_string = input()
+str = my_string
+print(len(str))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
